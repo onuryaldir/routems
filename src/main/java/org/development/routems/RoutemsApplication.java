2f6783fd24576911ccbbe35f9f5947eabbee76ff
Onur <onrdy96@gmail.com>
2024-10-29 14:20:48 +0300

    security poc

--- src/main/java/org/development/routems/RoutemsApplication.java
+++ src/main/java/org/development/routems/RoutemsApplication.java
@@ -3,8 +3,11 @@ package org.development.routems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableDiscoveryClient
 public class RoutemsApplication {
 
 	public static void main(String[] args) {
